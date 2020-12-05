5cb974e58b567508a5b80c5145330906047bfb85
Simon Lam <sl755@njit.edu>
2020-12-05 12:04:57 -0500

    rm useless fn

--- src/index.js
+++ src/index.js
@@ -13,12 +13,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// Callback for rerendering page
-// --- MIGHT BE REALLY INEFFICIENT ---
-export function rerender() {
-  //page.forceUpdate();
-}
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
